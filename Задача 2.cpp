cf686290c729c444a1cf567b34fb119be3ab2f32
Igor Achkas <jigoren@yandex.ru>
2023-10-15 22:53:32 +0700

    шаблонный класс для двумерного массива (таблицы).

--- Задача 2.cpp
+++ Задача 2.cpp
@@ -3,148 +3,107 @@
 #include <windows.h>
 #include <exception>
 
-
-class Unwanted_index : public std::exception
-{
-public:
-    const char* what() const override { return "Выход за пределы массива! Добавить элемент невозможно!"; }
-};
-
-
+template<class T>
 class arr
 {
-
 private:
+    int rows;
+    int cols;
+    T** table;  
 
-    int* element;
-    int index = 0;
-    int size;
 
 public:
-
-    arr(int size)
-    {       
-        element = new int[size]();
-        this->size = size;
-    }
-
-
-    auto add_element(int num) 
+    arr(int rows, int cols)
     {
-        if (index > size - 1) throw Unwanted_index();
-        element[index] = num;
-        index++;
-        return element[index];
-
-
-    }    
-
-    auto get_element(int index) 
-    {
-        if ((index > size - 1) || (index < 0)) throw("...");
-        return element[index];
-    }
-
-    auto get_size()
-    {
-        int S = size;
-        return S;
-    }
-
-
-    void print_arr(arr*) 
-    {
-        for (int i = 0; i < size; ++i)
-            std::cout << element[i] << " ";
-        std::cout << std::endl;
-    }
-    
-    arr& operator=(const arr& other)
-    {
-        if (this != &other)
+        table = (T**) new T* [rows]();        
+        for (int i = 0; i < rows; i++)
         {
-            delete[] element;
-            this->size = other.size;
-            this->index = other.index;
-            this->element = new int[other.size];
-            for (int i = 0; i < other.size; ++i)
-                this->element[i] = other.element[i];
+            table[i] = new int[cols]();
         }
-        return *this;
-    } 
+        this->rows = rows;
+        this->cols = cols;
 
-    
-    arr(const arr& other) 
-    {       
-            this->size = other.size;
-            this->index = other.index;
-            this->element = new int[other.size];
-            for (int i = 0; i < other.size; ++i)
-                this->element[i] = other.element[i];                 
+    }
+
+
+
+    T* operator[](T elem)
+    {
+        return table[elem];
+    }
+
+    const T* operator[](const T elem)const
+    {
+        return  table[elem];
+    }
+
+    T size(int r, int c)
+    {
+        if (r > 0)
+        {            
+            for (int i = 0; i < r; i++)
+                delete[] table[i];
+        }
+
+        if (c > 0)
+        {
+            delete[] table;
+        }
+       
+        this->rows = r;
+        this->cols = c;
+        
+        table = (T**) new T* [r](); 
+        for (int i = 0; i < r; i++)
+            table[i] = (T*) new T[c]();
+        
+        return **table;
+    }
+
+    void print_arr()
+    {
+        for (int i = 0; i < rows; i++) 
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                std::cout << table[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
     }
 
     ~arr()
-    {        
-        delete[] element;
-    }
+    {
+        if (cols > 0) {
+            for (int i = 0; i < rows; i++)
+                delete[] table[i];
+        }
 
+        if (rows > 0)
+            delete[] table;        
+    }
 };
+
+
+
+
 
 int main()
 {
     setlocale(LC_ALL, "ru");
     SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+    SetConsoleOutputCP(1251);      
 
-    try {
-        arr smart_array(6);
-        smart_array.add_element(99);
-        smart_array.add_element(77);
-        smart_array.add_element(88);
-        smart_array.add_element(66);
-        smart_array.add_element(33);
-        smart_array.add_element(22);
-       /* smart_array.add_element(55);*/
-
-        arr new_array(2);
-        new_array.add_element(909);
-        new_array.add_element(304);        
-
-        /*std::cout << smart_array.get_element(4) <<"*"<< smart_array.get_size() << std::endl;
-        std::cout << new_array.get_element(0) << "*" << new_array.get_size() << std::endl; */ 
-
-        new_array.print_arr(&new_array);
-        std::cout << std::endl;
-
-        smart_array.print_arr(&smart_array);
-        std::cout << std::endl;
-
-        arr smart_array_copy(smart_array);
-        smart_array_copy.print_arr(&smart_array_copy);
-        std::cout << std::endl;
-        
-
-        smart_array = new_array;
-
-      /*smart_array = smart_array;*/
-        
-
-        smart_array.print_arr(&smart_array);        
-
-    }
-
-    catch (const std::exception& ex) 
-    {
-        std::cout << ex.what() << std::endl;
-    }
-
-    catch (...)
-    {
-        std::cout << "Элемента с таким индексом не существует!" << std::endl;
-    }
+    auto test = arr<int>(6, 5);
+    test[3][3] = 4;
+    std::cout << test[3][3];
+    std::cout << std::endl;
+    test.print_arr();
+    test.size(3, 3);
+    std::cout << std::endl;
+    test[2][2] = 9;
+    std::cout << test[2][2] << std::endl;
+    test.print_arr();      
 
     return 0;
 }
-    
-
-

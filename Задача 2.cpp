3e89dd9867a3a329eee8584d9d1c443d15a5a61e
Igor Achkas <jigoren@yandex.ru>
2023-11-10 17:16:03 +0700

    Задача 2. Печать контейнера

--- Задача 2.cpp
+++ Задача 2.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿//Задача 2
+#include <iostream>
 #include <string>
 #include <windows.h>
 #include <vector>
@@ -6,38 +7,47 @@
 #include <array>
 #include <set>
 #include <map>
-#include <unordered_map>
+#include <list>
 
-
-
-
-
-
-int main()
-{
-    setlocale(LC_ALL, "ru");
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);      
-
-    std::cout << "[IN] : " << std::endl;   
-
-    std::array<int, 6> arr_i{ 1, 5, 1, 3, 4, 4 };
-
-    for (const auto& elem : arr_i)
-        std::cout << elem << std::endl;
-
-    std::cout << std::endl;
-
-    std::cout << "[OUT] : " << std::endl;
-
-    std::set<int> set_i;         
-
-    set_i.insert(arr_i.begin(), arr_i.end());
-    
-    std::for_each(set_i.rbegin(), set_i.rend(), [](int number)
-        {
-            std::cout << number << ' ' << std::endl;
-        });   
-
-    return 0;
+template <class T>
+ void print_container(T s, T e)
+{    
+     while ( s != e) 
+     {
+         std::cout << *s <<",";
+         ++s;
+     } 
+     std::cout << std::endl;
 }
+
+
+ template <class T>
+ void print2_container(const T& container)
+ { 
+     for (const auto & elem : container)
+                     std::cout << elem << std::endl;     
+     std::cout << std::endl;
+ }
+    int main()
+    {
+        setlocale(LC_ALL, "ru");
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+
+        std::vector<std::string> test_vector = { "one", "two", "three", "four" };         
+        print_container(test_vector.begin(),test_vector.end());
+        std::cout << std::endl;
+        print2_container(test_vector);        
+
+        std::set<std::string> test_set = { "one", "two", "three", "four" };        
+        print_container(test_set.begin(), test_set.end());
+        std::cout << std::endl;
+        print2_container(test_set);
+
+        std::list<std::string> test_list = { "one", "two", "three", "four" };        
+        print_container(test_list.begin(), test_list.end());
+        std::cout << std::endl;
+        print2_container(test_list);
+
+        return 0;
+    }
